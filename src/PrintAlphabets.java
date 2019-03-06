1c0fc837f902473b6633b6903b623a3446ff16dc
nkalapala <narmada_mikki@hotmail.com>
2019-03-06 12:55:15 +0000

    JavaEnglishAlphabets

---
+++ src/PrintAlphabets.java
@@ -0,0 +1,15 @@
+package love.programming;
+ class Alphabets
+{
+
+    public static void main(String args[])
+
+    {
+        char al;
+
+        for (al = 'A'; al <= 'Z'; al++)
+            System.out.println(al);
+    }
+
+
+}
